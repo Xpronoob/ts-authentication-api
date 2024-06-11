4eda5bb6516f974ae5f66a4277b814c5a8c6a48c
Xpronoob <kevin_03_puris@hotmail.com>
2024-06-10 19:02:36 -0600

    User dto, repository & datasource

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express'
+import { RegisterUserDto } from '../../domain'
 
 export class AuthController {
   // DI
   constructor () {}
 
   registerUser = (req: Request, res: Response) => {
-    res.json({
-      message: 'RegisterController'
-    })
+    const [error, registerUserDto] = RegisterUserDto.create(req.body)
+    if (error) return res.status(400).json({ error })
+
+    res.json(registerUserDto)
   }
 
   loginUser = (req: Request, res: Response) => {
