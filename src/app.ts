9c66c5fc0f86fd6ebaa94ad89bddd9868cdefafb
Xpronoob <kevin_03_puris@hotmail.com>
2024-06-17 23:55:34 -0600

    env mongo configuration & validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import { AppRoutes } from './presentation/routes'
 import { envs } from './config'
 import { Server } from './presentation/server'
+import { MongoDatabase } from './data/mongodb'
 
 (() => {
   main()
 })()
 
 async function main () {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
+
   new Server({
     port: envs.PORT,
     routes: AppRoutes.routes
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,22 +2,67 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const getPort = (): number | undefined => {
+export const getPort = (): number => {
   const port = process.env.PORT
-  try {
-    if (!port) {
-      throw new Error('PORT is not defined in the environment variables')
-    }
-    const num = Number.parseInt(port, 10)
-    if (isNaN(num)) {
-      throw new Error(`PORT is not a valid number: ${port}`)
-    }
-    return num
-  } catch (error) {
+  if (!port) {
+    throw new Error('PORT is not defined in the environment variables')
+  }
+  const num = Number.parseInt(port, 10)
+  if (isNaN(num)) {
+    throw new Error(`PORT is not a valid number: ${port}`)
+  }
+  return num
+}
 
+interface DBConnection {
+  url: string
+  dbName: string
+}
+
+export class MongoConnection implements DBConnection {
+  url: string
+  dbName: string
+
+  constructor () {
+    this.url = process.env.MONGO_URL || ''
+    this.dbName = process.env.MONGO_DB_NAME || ''
+    this.validateConnectionInfo()
+  }
+
+  private validateConnectionInfo (): void {
+    if (!this.url) {
+      throw new Error('MONGO_URL is not defined in the environment variables')
+    }
+
+    if (!this.dbName) {
+      throw new Error('MONGO_DB_NAME is not defined in the environment variables')
+    }
+  }
+
+  getMongoInfo (): { url: string, dbName: string } {
+    this.validateConnectionInfo()
+    return { url: this.url, dbName: this.dbName }
   }
 }
 
+// export const getMongo = (): DBConnection => {
+//   const url = process.env.MONGO_URL || ''
+//   const dbName = process.env.MONGO_DB_NAME || ''
+//   if (!url) {
+//     throw new Error('MONGO_URL is not defined in the environment variables')
+//   }
+//   if (!dbName) {
+//     throw new Error('MONGO_DB_NAME is not defined in the environment variables')
+//   }
+//   return { url, dbName }
+// }
+
+const dbInstance = new MongoConnection()
+
+const { url, dbName } = dbInstance.getMongoInfo()
+
 export const envs = {
-  PORT: getPort()
+  PORT: getPort(),
+  MONGO_URL: url,
+  MONGO_DB_NAME: dbName
 }
