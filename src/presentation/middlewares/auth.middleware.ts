6ca4d3a896271446baeec242c6e793f85ba08f37
Xpronoob <kevin_03_puris@hotmail.com>
2024-06-27 22:10:48 -0600

    Auth middleware validate token from cookies

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -3,13 +3,18 @@ import { JwtAdapter } from '../../config'
 import { error } from 'console'
 import { UserModel } from '../../data/mongodb'
 
+// todo: get token from cookies
 export class AuthMiddleware {
   static validateJWT = async (req: Request, res: Response, next: NextFunction) => {
-    const authorization = req.header('Authorization')
-    if (!authorization) return res.status(401).json({ error: 'No token provided' })
-    if (!authorization.startsWith('Bearer ')) return res.status(401).json({ error: 'Invalid Bearer token' })
-
-    const token = authorization.split(' ').at(1) || ''
+    // Get token from cookies
+    const token = req.cookies.token
+    if (!token) {
+      // Get token from header
+      const authorization = req.header('Authorization')
+      if (!authorization) return res.status(401).json({ error: 'No token provided' })
+      if (!authorization.startsWith('Bearer ')) return res.status(401).json({ error: 'Invalid Bearer token' })
+      const token = authorization.split(' ').at(1) || ''
+    }
 
     try {
       // todo:
