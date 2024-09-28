967e132a04be6c1bda3db2cd7e7b080e7b42daf3
Xpronoob <kevin_03_puris@hotmail.com>
2024-09-28 14:26:17 -0600

    UserMapper TEST

--- __test__/infrastructure/mappers/user.mapper.test.ts
+++ __test__/infrastructure/mappers/user.mapper.test.ts
@@ -5,15 +5,26 @@ import { UserEntity } from '../../../src/domain/entities/user.entity'
 describe('User Mapper', () => {
   test('should map user to user entity', () => {
     const input = {
-      _id: 'id_test',
+      id: 'id_test',
       name: 'name',
       email: 'email@email.com',
-      password: 'Password1!',
       roles: ['roles', 'roles'],
+      password: 'Password1!',
       img: 'img',
     }
 
-    const expected = new UserEntity(input._id, input.name, input.email, input.password, input.roles, input.img)
+    const expected = new UserEntity(
+      input.id,
+      input.name,
+      input.email,
+      input.roles,
+      input.password,
+      undefined,
+      undefined,
+      input.img,
+      undefined,
+      undefined,
+    )
 
     const result = UserMapper.userEntityFromObject(input)
 
